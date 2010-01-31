0ffdc766b225aed0ed1bb2ecbc084e7aaecd505e
Ewen Cheslack-Postava <ewencp@cs.stanford.edu>
2010-01-31 15:10:53 -0800

    Add support for finding URLs in tweets, checking if they are short URLs (301 redirects), and replacing with the full link in the text as well as storing the short link in a short_urls field.

--- fetch.py
+++ fetch.py
@@ -1,7 +1,8 @@
+#!/usr/bin/python
 import warnings
 warnings.simplefilter('ignore', DeprecationWarning)
 
-import httplib2, urllib, time
+import httplib2, urllib, time, re
 try:
     import json
 except ImportError:
@@ -21,6 +22,51 @@ def load_all():
     except IOError:
         return []
 
+def normalize_url(url):
+    # Simple length heuristic
+    if len(url) < 10: return None
+
+    # Make sure we have some sort of protocol
+    if not re.search('://', url):
+        url = 'http://' + url
+
+    return url
+
+def lookup_short_urls(tweet):
+    # If short_urls are already there, skip
+    if 'short_urls' in tweet: return
+
+    # (Start of line or word)
+    # (Maybe something like http://)
+    # (A vaguely domain-like section, at least one dot which is not a double dot)
+    # (Whatever else follows, liberally via non-whitespace)
+    url_regex = '(\A|\\b)([\w-]+://)?\S+[.][^\s.]\S*'
+
+    redir = httplib2.Http(timeout=10)
+    redir.follow_redirects = False
+    redir.force_exception_to_status_code = True
+
+    short_urls = {}
+
+    new_text = tweet['text']
+    for sub in tweet['text'].split():
+        orig_url_match = re.search(url_regex, sub)
+        if not orig_url_match:
+            continue
+        orig_url = normalize_url(orig_url_match.group(0))
+        if not orig_url: continue
+
+        try:
+            response = redir.request(orig_url)[0]
+            if 'status' in response and response['status'] == '301':
+                short_urls[response['location']] = orig_url
+                new_text = new_text.replace(orig_url, response['location'])
+        except:
+            pass
+
+    tweet['short_urls'] = short_urls
+    tweet['text'] = new_text
+
 def fetch_and_save_new_tweets():
     tweets = load_all()
     old_tweet_ids = set(t['id'] for t in tweets)
@@ -35,10 +81,11 @@ def fetch_and_save_new_tweets():
             tweets.append(tweet)
             num_new_saved += 1
     tweets.sort(key = lambda t: t['id'], reverse=True)
-    # Delete the 'user' key
+    # Delete the 'user' key, lookup short URLs
     for t in tweets:
         if 'user' in t:
             del t['user']
+        lookup_short_urls(t)
     # Save back to disk
     json.dump(tweets, open(FILE, 'w'), indent = 2)
     print "Saved %s new tweets" % num_new_saved
@@ -50,9 +97,9 @@ def fetch_all(since_id = None):
     args = {'count': 200}
     if since_id is not None:
         args['since_id'] = since_id
-    
+
     all_tweets_len = len(all_tweets)
-    
+
     while True:
         args['page'] = page
         headers, body = h.request(
@@ -71,7 +118,7 @@ def fetch_all(since_id = None):
         #print "Fetched another %s" % (len(all_tweets) - all_tweets_len)
         all_tweets_len = len(all_tweets)
         time.sleep(2)
-    
+
     all_tweets.sort(key = lambda t: t['id'], reverse=True)
     return all_tweets
 
